6eec9d5fe621af1409fdcaa228f61c25e5ee7b2b
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2022-10-03 12:25:57 +0400

    Initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import user from '../user.json';
+import UserProfile from './UserProfile/UserProfile';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <UserProfile {...user} />
     </div>
   );
 };
